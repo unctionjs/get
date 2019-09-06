78fbdcc8941b0ee6380bf5ba1f71f53286e194d1
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:46 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-undefined, no-magic-numbers */
+import get from "./index.ts";
+
+test("Object found key", () => {
+  expect(get("aaa")({aaa: "1"})).toEqual("1");
+});
+
+test("Object missing key", () => {
+  expect(get("bbb")({aaa: "1"})).toEqual(undefined);
+});
+
+test("undefined missing key", () => {
+  expect(get("bbb")(undefined)).toEqual(undefined);
+});
+
+test("Array missing key", () => {
+  expect(get(0)(["aaa"])).toEqual("aaa");
+});
+
+test("String found key", () => {
+  expect(get(0)("abc")).toEqual("a");
+});
+
+test("Map found key", () => {
+  expect(get("aaa")(new Map([["aaa", "aaa"]]))).toEqual("aaa");
+});
